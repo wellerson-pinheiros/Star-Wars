fb2bfd34d004f30560b47e52836dcd12e993bc17
wellerson pinheiros <wellersonpinheiros@outlook.com>
2025-05-28 18:04:04 -0300

    :sparkles: feat: configurando o banco de dados

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [ConfigModule.forRoot({
+      isGlobal: true, // permite acesso ao process.env em toda a aplicação
+         }),
+    TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '3306'),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [],
+    synchronize:true,
+  })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  process.env.TZ = "-03:00"
+  await app.listen(process.env.PORT ?? 4000);
+  app.enableCors()
+  app.useGlobalPipes(new ValidationPipe())
+}
+bootstrap();
